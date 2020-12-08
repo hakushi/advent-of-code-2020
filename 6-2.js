d3d047d8629a7647622a5868684302c12b919528
David Edström <david.edstrom@pricerunner.com>
2020-12-08 11:23:00 +0100

    day 6 part 3

---
+++ 6-2.js
@@ -0,0 +1,34 @@
+const { forEach } = require('./6-data');
+const data = require('./6-data');
+
+const getYesCountFromGroup = group => {
+    const answers = {};
+    const groupSize = group.length;
+    let count = 0;
+
+    group.forEach(answer => {
+        answer.split('').forEach(letter => {
+            if(!answers.hasOwnProperty(letter)) {
+                answers[letter] = 1;
+            } else {
+                answers[letter] += 1
+            }
+        } )
+    });
+
+    Object.keys(answers).forEach(key => {
+        if(answers[key] === groupSize) {
+            count++;
+        }
+    })
+
+    return count
+}
+
+let count = 0;
+data.forEach(group => {
+    count += getYesCountFromGroup(group);
+});
+
+console.log('Count: ', count);
+
